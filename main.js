2ffd140e51d895a0da17154dc9847d892d9bfd5e
Tobiasverbeek <302747159@student.rocmondriaan.nl>
2023-09-01 12:39:23 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+let a = 8
+let b = 7
+
+console.log("optellen",a+b);
+console.log("aftrekken",a-b);
+console.log("keer elkaar",a*b);
+console.log("aftrekken",b-a);
+console.log("delen door",a/b);
+
+let x = 4
+let y = 2
+
+console.log("optellen",x+y);
+console.log("aftrekken",x-y);
+console.log("keer elkaar",x*y);
+console.log("aftrekken",y-x);
+console.log("delen door",x/y);
+
+let salary = 2000
+let raise = 0.05
+
+console.log("keer elkaar",raise*salary);
+
+
+x = 4;
+y = 0;
+
+console.log("delen door",x/y);
+
+let huis = 400000;
+let inflatie = 0.025;
+
+console.log("inflatie prijs", huis*inflatie)
+
+let jas = 100
+let btw = 1.21
+
+console.log("extra btw", jas*btw)
